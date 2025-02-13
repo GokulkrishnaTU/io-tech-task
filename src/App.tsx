6906f3b539ac004eaf8c492e9cf87df54dfd673d
Gokul Krishna <gokulkrishnatu11@gmail.com>
2025-02-13 16:22:40 +0600

    initial commit with installed packages and cleanup projects

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-blue-600">heyy IO tech</h1>
+    </div>
+  );
+}
+
+export default App;
